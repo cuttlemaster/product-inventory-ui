772888bf9b6cf25b883a4397473b2b2492d92211
Caleb Flanagan <flanagan.caleb@gmail.com>
2025-08-19 10:56:05 -0600

    connected jest.setup.ts to the tsconfig.json
    The Typescript config wasn’t looking for the jest.setup.ts file, so we’ve added it there to be referenced during type-check commands.
    
    Also added an eslint plugin to fix a linter error in my IDE.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-const nextJest = require('next/jest')
+import nextJest from 'next/jest'
 
 const createJestConfig = nextJest({
   dir: './',
@@ -27,4 +27,4 @@ const customJestConfig = {
   ],
 }
 
-module.exports = createJestConfig(customJestConfig)
+export default createJestConfig(customJestConfig)
