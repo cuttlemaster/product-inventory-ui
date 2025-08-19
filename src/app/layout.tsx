a35fcfd64e65f0529915b894e0465cfb36f6dd5f
Caleb Flanagan <flanagan.caleb@gmail.com>
2025-08-19 15:40:46 -0600

    linked Product Inventory to index to fix accessibility
    Realized the top nav wasn’t keyboard navigable, so I made the three entries linked so they can be navigated that way.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
               <div className="flex justify-between h-16">
                 <div className="flex items-center">
                   <h1 className="text-xl font-semibold text-gray-900">
-                    Product Inventory
+                    <Link href="/">Product Inventory</Link>
                   </h1>
                 </div>
                 <div className="flex items-center space-x-4">
